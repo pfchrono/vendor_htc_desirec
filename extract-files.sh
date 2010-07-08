9d87e4ecad625f06e3970ff79ef29e120844adbd
James <jamezelle1@gmail.com>
2010-07-07 20:15:55 -0400

    removed proprietary libs that will be pulled with extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -23,6 +23,7 @@ adb pull /system/lib/libhtc_acoustic.so proprietary/libhtc_acoustic.so
 adb pull /system/lib/libhtc_ril.so proprietary/libhtc_ril.so
 adb pull /system/lib/libmm-adspsvc.so proprietary/libmm-adspsvc.so
 adb pull /system/lib/liboemcamera.so proprietary/liboemcamera.so
+adb pull /system/lib/libcamera.so proprietary/libcamera.so
 adb pull /system/lib/libOmxH264Dec.so proprietary/libOmxH264Dec.so
 adb pull /system/lib/libOmxMpeg4Dec.so proprietary/libOmxMpeg4Dec.so
 adb pull /system/lib/libOmxVidEnc.so proprietary/libOmxVidEnc.so
